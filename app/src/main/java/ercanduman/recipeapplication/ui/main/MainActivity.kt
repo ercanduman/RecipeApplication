7d26f429ab7a44462f79434265996a64b26a61d3
ercan.duman <ercanduman30@gmail.com>
2022-12-21 14:57:54 +0100

    UI LAYER - Add Navigation Component implementation into MainActivity.kt Introduce nav_graph.xml file to store app navigation

--- app/src/main/java/ercanduman/recipeapplication/ui/main/MainActivity.kt
+++ app/src/main/java/ercanduman/recipeapplication/ui/main/MainActivity.kt
@@ -2,42 +2,24 @@ package ercanduman.recipeapplication.ui.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ercanduman.recipeapplication.common.theme.RecipeApplicationTheme
+import ercanduman.recipeapplication.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            RecipeApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+
+        setupAppNavigation()
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    RecipeApplicationTheme {
-        Greeting("Android")
+    /**
+     * The recommendation for mixed Compose and Views apps is to use the Fragment-based Navigation
+     * component. Fragments will then hold View-based screens, Compose screens, and screens that use
+     * both Views and Compose. Once each Fragment's contents are in Compose, the next step is to tie
+     * all of those screens together with Navigation Compose and remove all of the Fragments.
+     *
+     * Details: https://developer.android.com/jetpack/compose/navigation#interoperability
+     */
+    private fun setupAppNavigation() {
     }
 }
