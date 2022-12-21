2e3e21cab66e52ea40d7fb7a3b564628063602c0
ercan.duman <ercanduman30@gmail.com>
2022-12-21 15:35:21 +0100

    UI LAYER - Add navigation to RecipeListFragment.kt and display RecipeDetailFragment

--- app/src/main/java/ercanduman/recipeapplication/ui/recipe/list/RecipeListFragment.kt
+++ app/src/main/java/ercanduman/recipeapplication/ui/recipe/list/RecipeListFragment.kt
@@ -4,10 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import ercanduman.recipeapplication.R
 
 class RecipeListFragment : Fragment() {
 
@@ -23,6 +33,20 @@ class RecipeListFragment : Fragment() {
 
     @Composable
     private fun FragmentContent() {
-        Text(text = "Recipe List fragment.")
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Recipe List fragment.")
+
+            Button(
+                modifier = Modifier
+                    .width(180.dp)
+                    .height(50.dp),
+                onClick = { findNavController().navigate(R.id.action_navigate_to_recipeDetailFragment) }
+            ) {
+                Text(text = "Navigate to Details")
+            }
+        }
     }
 }
