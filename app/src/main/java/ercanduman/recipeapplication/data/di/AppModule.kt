8c05887c9ad189fd2441282f53ad48bd182b0e9a
ercan.duman <ercanduman30@gmail.com>
2022-12-25 16:46:24 +0100

    DI - Create AppModule.kt and let Hilt know how to initialize RecipeService and RecipeRepository instances

---
+++ app/src/main/java/ercanduman/recipeapplication/data/di/AppModule.kt
@@ -0,0 +1,41 @@
+package ercanduman.recipeapplication.data.di
+
+import dagger.Module
+import dagger.Provides
+import ercanduman.recipeapplication.data.api.RecipeService
+import ercanduman.recipeapplication.data.repository.RecipeRepository
+import ercanduman.recipeapplication.data.repository.RecipeRepositoryImpl
+import javax.inject.Singleton
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+object AppModule {
+
+    private const val API_BASE_URL = "https://food2fork.ca/api/recipe/"
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRecipeService(
+        retrofit: Retrofit
+    ): RecipeService {
+        return retrofit.create(RecipeService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideRecipeRepository(
+        recipeService: RecipeService
+    ): RecipeRepository {
+        return RecipeRepositoryImpl(recipeService)
+    }
+}
