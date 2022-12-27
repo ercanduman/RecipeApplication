86b35fc20a372cb208e44f67f8e4b76394058bab
ercan.duman <ercanduman30@gmail.com>
2022-12-27 22:07:54 +0100

    RECIPE_LIST - Introduce RecipeItemComposable.kt compose component

---
+++ app/src/main/java/ercanduman/recipeapplication/ui/recipe/list/compose/RecipeItemComposable.kt
@@ -0,0 +1,11 @@
+package ercanduman.recipeapplication.ui.recipe.list.compose
+
+import androidx.compose.runtime.Composable
+import ercanduman.recipeapplication.domain.model.Recipe
+
+@Composable
+fun RecipeItemComposable(
+    recipe: Recipe,
+    onRecipeClick: () -> Unit
+) {
+}
