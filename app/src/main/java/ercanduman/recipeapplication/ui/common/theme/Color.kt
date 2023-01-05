e5185a08ef3d6f0c88939ee7a4c4bf97903d012a
ercan.duman <ercanduman30@gmail.com>
2023-01-05 15:15:39 +0100

    THEMING - Set Chip colors

--- app/src/main/java/ercanduman/recipeapplication/ui/common/theme/Color.kt
+++ app/src/main/java/ercanduman/recipeapplication/ui/common/theme/Color.kt
@@ -3,13 +3,17 @@ package ercanduman.recipeapplication.ui.common.theme
 import androidx.compose.ui.graphics.Color
 
 val AppColorPrimary = Color(0xFF049AE5)
-val AppColorPrimaryDark = Color(0xFF006CB2)
+val AppColorOnPrimary = Color(0xFF000000)
+val AppColorPrimaryContainer = Color(0xFF006db3)
+val AppColorOnPrimaryContainer = Color(0xFFFFFFFF)
 val AppColorPrimaryLight = Color(0xFF63CBFF)
 val AppColorPrimaryVariant = Color(0xFF28B5F6)
 
-val AppColorSecondary = Color(0xFFFA8A04)
-val AppColorLightPink = Color(0XFFECF7FD)
+val AppColorOnSecondary = Color(0xFFFFFFFF)
+val AppColorSecondaryContainer = Color(0xFF26c6da)
+val AppColorSecondary = Color(0xFF0095a8)
+val AppColorOnSecondaryContainer = Color(0xFF000000)
 
 val AppColorWhite = Color(0xFFFFFFFF)
 val AppColorDarkGrey = Color(0xFF333333)
-val AppColorBackgroundGrey = Color(0XFFF2F2F2)
+val AppColorBackgroundGrey = Color(0XFFF4F4F4)
